548d0d43f551912bae7603beed358107bef053d2
fireclouu <frcl@frcl.online>
2024-08-07 16:38:58 +0800

    cpp: implement LeetCode "IsSubsequence" challenge

---
+++ cpp/IsSubsequence.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+bool isSubsequence(std::string s, std::string t) {
+  if (t.size() < s.size()) return false;
+
+  int satisfy = 0;
+
+  for (char item : t) {
+    if (item == s[satisfy]) satisfy++;
+  }
+
+  return satisfy == s.size();
+}
+int main() {
+  std::string input = "ahsbbdcjfn";
+  std::string subseq = "axxbc";
+  std::cout << isSubsequence(subseq, input) << std::endl;
+  return 0;
+}
+
